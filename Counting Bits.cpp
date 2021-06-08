fec5132dcf07173646b4dc08e5a4d2ea2b31c325
Jasleen kaur <jasleen5900@gmail.com>
2021-06-09 01:57:07 +0530

    Create Counting Bits

---
+++ Counting Bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+    //Odd Even Checking  
+    if(n == 0) return {0};
+    vector<int> res(n + 1, 0);
+    res[0] = 0;
+    res[1] = 1;
+    
+    for(int i = 2; i <= n; i++)
+        res[i] = res[i / 2] + i % 2;
+    
+    return res;
+    }
+};
